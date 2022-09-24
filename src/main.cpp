7515727ca2e4347ac510228fc5b93f2d461b2a60
Niwol <lowin000@gmail.com>
2022-09-24 20:23:05 +0200

    Start removing qt and setting up glfw

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,42 @@
-#include "mainwindow.h"
+#include <GL/gl.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
 
-#include <QApplication>
+void framebuffer_size_callback(GLFWwindow* window, int width, int height); 
 
 int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
 
-    return a.exec();
+  std::cout << "Hello OpenGL!" << std::endl;
+
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow *window = glfwCreateWindow(800, 600, "Hello OpenGL", NULL, NULL);
+  if(window == nullptr) {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+
+    return EXIT_FAILURE;
+  }
+
+  glfwMakeContextCurrent(window);
+  glViewport(0, 0, 800, 600);
+
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+
+  return 0;
 }
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
